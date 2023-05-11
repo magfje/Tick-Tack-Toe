323aaef33a63a5766319385fd8bc16d7b75d4f00
Magnus Fjelldal <122229520+magfje@users.noreply.github.com>
2023-05-11 14:17:49 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using System.Text;
+using static System.Console;
+
+namespace Tick_Tack_Toe;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        OutputEncoding = Encoding.UTF8;
+
+        //Make more dynamic with nested loop?
+        var square00 = new Square(0, 0, true, null);
+        var square01 = new Square(6, 0, false, null);
+        var square02 = new Square(12, 0, false, null);
+        var square10 = new Square(0, 3, false, null);
+        var square11 = new Square(6, 3, false, null);
+        var square12 = new Square(12, 3, false, null);
+        var square20 = new Square(0, 6, false, null);
+        var square21 = new Square(6, 6, false, null);
+        var square22 = new Square(12, 6, false, null);
+
+        var array = new Square[3, 3]
+        {
+            { square00, square01, square02 },
+            { square10, square11, square12 },
+            { square20, square21, square22 }
+        };
+        var x = 0;
+        var y = 0;
+        ConsoleKey keyPressed;
+        do
+        {
+            foreach (var a in array)
+            {
+                a.Draw();
+                a.IsSelected = false;
+            }
+
+            //DisplayOptions();
+            var keyInfo = ReadKey(true);
+            keyPressed = keyInfo.Key;
+
+            //update SelectedIndex based on arrow keys
+            if (keyPressed == ConsoleKey.LeftArrow)
+            {
+                x--;
+                if (x == -1) x = 2;
+            }
+            else if (keyPressed == ConsoleKey.RightArrow)
+            {
+                x++;
+                if (x == 3) x = 0;
+            }
+            else if (keyPressed == ConsoleKey.UpArrow)
+            {
+                y--;
+                if (y == -1) y = 2;
+            }
+            else if (keyPressed == ConsoleKey.DownArrow)
+            {
+                y++;
+                if (y == 3) y = 0;
+            }
+            else if (keyPressed == ConsoleKey.Enter)
+            {
+                array[y, x].UserPick();
+                BotPicker();
+            }
+            else if (keyPressed == ConsoleKey.M)
+            {
+                array[y, x].BotPick();
+            }
+
+            if (array[y, x].IsSelected == false)
+                array[y, x].IsSelected = true;
+            //else
+            //    array[y, x].IsSelected = false;
+
+            Clear();
+        } while (keyPressed != ConsoleKey.Escape);
+
+        void BotPicker()
+        {
+            var y = new Random().Next(0, 2);
+            var x = new Random().Next(0, 2);
+            if (array[y, x].IsUser == null) array[y, x].BotPick();
+        }
+        //var a = new Square(0, 1, false, null);
+        //a.Draw();
+        //var b = new Square(6, 1, false, null);
+        //b.Draw();
+    }
+}
---
+++ Square.cs
@@ -0,0 +1,59 @@
+﻿namespace Tick_Tack_Toe;
+
+using static Console;
+
+public class Square
+{
+    public Square(int x, int y, bool isSelected, bool? isUser)
+    {
+        X = x;
+        Y = y;
+        IsSelected = isSelected;
+        IsUser = isUser;
+    }
+
+    public int X { get; set; }
+    public int Y { get; set; }
+    public bool IsSelected { get; set; }
+    public bool? IsUser { get; set; }
+
+    public void Draw()
+    {
+        var sqXorO = IsUser switch
+        {
+            null => "  ",
+            true => "👻",
+            false => "🤖"
+        };
+
+        const string sqTop = "┌────┐";
+        var sqMid = $"│ {sqXorO} │";
+        const string sqBot = "└────┘";
+
+
+        //string[] sq = { "┌─────┐", "│     │", "│     │", "└─────┘" };
+        string[] sq = { sqTop, sqMid, sqBot };
+
+
+        var y = Y;
+        foreach (var p in sq)
+        {
+            if (IsSelected) ForegroundColor = ConsoleColor.Green;
+
+            SetCursorPosition(X, y);
+            Write(p);
+            y++;
+            ResetColor();
+        }
+    }
+
+    public void UserPick()
+    {
+        IsUser ??= true;
+    }
+
+    public void BotPick()
+    {
+        IsUser ??= false;
+    }
+}
